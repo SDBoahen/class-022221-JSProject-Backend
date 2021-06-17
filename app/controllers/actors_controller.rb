54fb3fc6816827377f6200ee6fb99ca3a50cf0aa
SDBoahen <SDBoahen@gmail.com>
2021-06-17 13:01:00 -0400

    END OF 6-17-2021

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -16,4 +16,34 @@ class ActorsController < ApplicationController
     end
 
 
+    def update
+        @actor = Actor.find_by_id(params[:id])
+        if @actor.update(actor_params)
+          render json: @actor
+        else
+          render json: @actor.errors, status: :unprocessable_entity
+        end
+    end
+
+
+    # DELETE /toys/1
+    def destroy
+        @actor.destroy
+    end
+
+
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_toy
+      @actor = Actor.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def actor_params
+      params.require(:actor).permit(:name, :image, :likes, :on_stage, :comment)
+    end
+
+
 end
